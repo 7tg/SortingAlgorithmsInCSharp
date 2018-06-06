b78bd85f8785d6fec7bfc98a31b850637e0bd5f5
tayyipgoren <tayyipgoren@gmail.com>
2018-06-06 18:15:26 +0300

    Added Merge Sort

---
+++ SortingAlgorithms/MergeSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SortingAlgorithms
+{
+    public static class MergeSort
+    {
+        public static long Sort(int[] array)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            SortMerge(array,0,array.Length);
+            
+            stopwatch.Stop();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        public static void SortMerge(int[] input, int start, int end)
+        {
+            if (end - start < 2) return;
+
+            int mid = (start + end) / 2;
+            
+            SortMerge(input,start,mid);
+            SortMerge(input,mid,end);
+            Merge(input, start, mid, end);
+
+        }
+
+        public static void Merge(int[] input, int start, int mid, int end) {
+
+            if (input[mid - 1] <= input[mid]) {
+                return;
+            }
+
+            int i = start;
+            int j = mid;
+            int tempIndex = 0;
+
+            int[] temp = new int[end - start];
+            while (i < mid && j < end) {
+                temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+            }
+            Array.Copy(input, i, input, start + tempIndex, mid - i);
+            Array.Copy(temp, 0, input, start, tempIndex);
+
+            }
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -9,16 +9,21 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
-            int arraySize = 10000;
+            int arraySize = 1000000;
             
             RandomArray arr = new RandomArray(arraySize);
             arr.FillArrayRandom();
 
-            Console.WriteLine("Size of the shuffled array :"+arraySize);
-            Console.WriteLine("Bubble Sort Took(miliseconds):"+BubbleSort.Sort(arr.Clone()));
-            Console.WriteLine("Selection Sort Took(miliseconds):"+SelectionSort.Sort(arr.Clone()));
-            Console.WriteLine("Insertion Sort Took(miliseconds):"+InsertionSort.Sort(arr.Clone()));
+            if (arraySize < 10001)
+            {
+                Console.WriteLine("Size of the shuffled array :"+arraySize);
+                Console.WriteLine("Bubble Sort Took(miliseconds):"+BubbleSort.Sort(arr.Clone()));
+                Console.WriteLine("Selection Sort Took(miliseconds):"+SelectionSort.Sort(arr.Clone()));
+                Console.WriteLine("Insertion Sort Took(miliseconds):"+InsertionSort.Sort(arr.Clone()));
+            }
+            
             Console.WriteLine("Shell Sort Took(miliseconds):"+ShellSort.Sort(arr.Clone()));
+            Console.WriteLine("Merge Sort Took(miliseconds):"+MergeSort.Sort(arr.Clone()));
 
 
         }
